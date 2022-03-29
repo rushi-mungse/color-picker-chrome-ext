b01fe33b326aea52c441db6c47a238fb138c773f
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-03-29 09:44:45 +0530

    color picker

--- popup.js
+++ popup.js
@@ -1,15 +1,38 @@
 const btn = document.querySelector(".changeColorBtn");
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
 
 btn.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id,
-      function: pickColor,
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: codePicker,
     },
-  });
+    async (injectionResult) => {
+      const [data] = injectionResult;
+      if (data.result) {
+        const color = data.result.sRGBHex;
+        colorGrid.style.backgroundColor = color;
+        colorValue.innerText = color;
+        try {
+          await navigator.clipboard.writeText(color);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  );
 });
 
-const pickColor = () => {
-  console.log("script");
-};
+async function codePicker() {
+  try {
+    //picker
+    const eyeDropper = new EyeDropper();
+    return await eyeDropper.open();
+  } catch (error) {
+    console.log(error);
+  }
+}
