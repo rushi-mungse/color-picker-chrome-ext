e47b1b846c517c012330e202b6b0e7fad5a34dd2
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-03-29 08:49:30 +0530

    color picker ext

---
+++ popup.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector(".changeColorBtn");
+
+btn.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: {
+      tabId: tab.id,
+      function: pickColor,
+    },
+  });
+});
+
+const pickColor = () => {
+  console.log("script");
+};
